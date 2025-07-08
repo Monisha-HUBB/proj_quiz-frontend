298029065c987636a98f85e4eadcb3bcb3cc95a7
Monisha-HUBB <monishakuttyrani@gmail.com>
2025-07-08 16:58:09 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TakeQuizPage from './pages/TakeQuizPage';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Quiz App</h1>
+      <TakeQuizPage />
+    </div>
+  );
+}
+
+export default App;
